735cf4875b1d9bd014485a2e13dd5e5a7f4ad894
Ile Eftimov <ileeftimov@gmail.com>
2014-02-27 23:53:27 +0100

    fix commic_summary

--- lib/marver/entities/summaries/comic_summary.rb
+++ lib/marver/entities/summaries/comic_summary.rb
@@ -2,11 +2,11 @@ require './lib/marver.rb'
 
 module Marver
   class ComicSummary
-    attr_reader :id, :title, :resource_uri, :type
+    attr_reader :id, :name, :resource_uri, :type
 
     def initialize(json, credentials)
       @credentials = credentials
-      @title = json['title']
+      @name = json['name']
       @resource_uri = "#{json['resourceURI']}?#{@credentials.to_s}"
       @id = json['id'].to_i
     end
