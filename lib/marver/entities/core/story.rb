815b8e53ffd30d43c1865f569a1a7e669c8d4a2a
Ile Eftimov <ileeftimov@gmail.com>
2014-03-01 16:07:16 +0100

    story has a title, not a name; specs GREEN

--- lib/marver/entities/core/story.rb
+++ lib/marver/entities/core/story.rb
@@ -1,9 +1,12 @@
 module Marver
   class Story
+    attr_reader :title, :type, :resource_uri
 
     def initialize(data, credentials)
-
-
+      @credentials = credentials
+      @title = data['title']
+      @type = data['type']
+      @resource_uri = "#{data['resourceURI']}?#{@credentials.to_s}"
     end
   end
 end
--- spec/marver/entities/core/story_spec.rb
+++ spec/marver/entities/core/story_spec.rb
@@ -6,21 +6,22 @@ describe Marver::Story do
   describe "Attributes" do
 
     let(:credentials) { Marver::Credentials.new("priv_key", 'pub_key') }
-    let(:story) { Marver::Story.new({
-              "resourceURI" => "http://gateway.marvel.com/v1/public/stories/703",
-              "name" => "Interior #703", "type" => "interiorStory" }, credentials)
-                }
+    let(:json) { fixture('story.json').read }
+    let(:response) { Marver::REST::Response.new(json) }
+    let(:data_container) { Marver::DataContainer.new(response) }
+    let(:story) { Marver::Story.new(data_container.results, credentials) }
 
-    it "#name - The story title." do
-      expect(story.name).to eq "Interior #703"
+    it "#title - The story title." do
+      expect(story.title).to eq "Cover #892"
     end
 
     it "#type - The story type e.g. interior story, cover, text story." do
-      expect(story.type).to eq "interiorStory"
+      expect(story.type).to eq "cover"
     end
 
     it "#resource_uri - The canonical URL identifier for this resource." do
-      expect(story.resource_uri).to eq "http://gateway.marvel.com/v1/public/stories/703"
+      Time.stub_chain(:now, :to_i, :to_s).and_return "1"
+      expect(story.resource_uri).to eq "http://gateway.marvel.com/v1/public/stories/892?ts=1&apikey=pub_key&hash=3d4ce88a477c7e4a5accbf6cd2c8b819"
     end
 
   end
